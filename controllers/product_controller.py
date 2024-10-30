fe46a9efea5b4a411db8e1698582761bbfc11ac1
joaquin_n6 <joaquinne87@gmail.com>
2024-10-29 22:05:24 -0600

    get by id product

--- controllers/product_controller.py
+++ controllers/product_controller.py
@@ -1,8 +1,9 @@
 """Routes y controllers de usuarios"""
-from fastapi import APIRouter, Depends, status, Body
+from fastapi import APIRouter, Depends, HTTPException, status, Body
 from fastapi.security import HTTPAuthorizationCredentials
 from core.auth import AuthService, OptionalHTTPBearer
 from core import var_mongo_provider as mongo_provider
+from models.product_model import Product
 from services.product_service import ProductService
 from schemas.product_schema import ProductCreate, ProductCreateResponse
 
@@ -26,3 +27,20 @@ async def product_post(token: HTTPAuthorizationCredentials = Depends(auth_scheme
   service = ProductService(mongo_provider.db)
   new_product = service.create_product(product)
   return new_product.model_dump()
+
+
+@router.get(
+    "/products/{product_id}",
+    status_code=status.HTTP_201_CREATED,
+    summary="Get product by id"
+)
+async def product_get_by_id(product_id: str) -> Product:
+  entity = mongo_provider.db.products.find_one({'_id': product_id})
+  if not entity:
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Product not found",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+  product = Product(id=entity['_id'], **entity)
+  return product.model_dump()

a4beb4526bf6ffe78e751d3c33c1b9c93d55115f
ErickRuiz97 <33972753+ErickRuiz97@users.noreply.github.com>
2024-12-02 19:55:47 -0600

    CRUD-USERS

--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -5,7 +5,9 @@ from core import helpers_api, var_mongo_provider as mongo_provider
 from core.auth import AuthService, OptionalHTTPBearer
 from services.user_service import UserService
 from models.token_model import Token
-from schemas.user_schema import UserCreate, UserLogin, UserResponse
+from schemas.user_schema import UserCreate, UserLogin, UserResponse, UserQuery, UserListResponse, UserUpdate
+from models.user_model import UserInfo
+from fastapi import Query
 
 auth_scheme = OptionalHTTPBearer()
 
@@ -27,6 +29,48 @@ async def user_post(token: HTTPAuthorizationCredentials = Depends(auth_scheme), 
   service = UserService(mongo_provider.db)
   new_user = service.create_user(user)
   return new_user.model_dump(by_alias=True)
+
+@router.get(
+    "/users/{user_id}",
+    status_code=status.HTTP_200_OK,
+    summary="Get user by id"
+)
+async def user_get_by_id(user_id: str) -> UserInfo:
+  entity = mongo_provider.db.users.find_one({'_id': user_id})
+  if not entity:
+    helpers_api.raise_error_404('User')
+  user = UserInfo(**entity);
+  return user.model_dump(by_alias=True)
+
+@router.put(
+    "/users/{user_id}",
+    status_code=status.HTTP_200_OK,
+    summary="Put user by id"
+)
+async def user_update_by_id(
+        user_id: str,
+        user: UserUpdate = Body(...),
+        token: HTTPAuthorizationCredentials = Depends(auth_scheme)) -> UserUpdate:
+  if not AuthService().is_manager(token):
+    helpers_api.raise_no_authorized()
+  entity = mongo_provider.db.users.find_one({'_id': user_id})
+  if not entity:
+    helpers_api.raise_error_404('User')
+  service = UserService(mongo_provider.db)
+  update_user = service.update_user(user_id, user)
+  return update_user.model_dump(by_alias=True)
+
+@router.get(
+    "/users",
+    status_code=status.HTTP_200_OK,
+    summary="Get all users"
+)
+async def get_users(query_params: UserQuery = Query(...)) -> UserListResponse:
+  service = UserService(mongo_provider.db)
+  query, pagination = service.get_query(query_params)
+  products = UserListResponse(
+      **helpers_api.get_paginator('users', query, pagination))
+  return products.model_dump(by_alias=True)
 
 
 @router.post(
--- models/user_model.py
+++ models/user_model.py
@@ -3,10 +3,18 @@ from datetime import datetime
 
 
 class User(BaseModel):
-  id: str = None
+  id: str = Field(..., alias="_id")
   email: str = Field(...)
   roles: list[str] = Field(...)
   full_name: str = Field(...)
   password: str = Field(...)
+  created_at: datetime = None
+  updated_at: datetime = None
+
+class UserInfo(BaseModel):
+  id: str = Field(..., alias="_id")
+  email: str = Field(...)
+  roles: list[str] = Field(...)
+  full_name: str = Field(...)
   created_at: datetime = None
   updated_at: datetime = None

020bc2a2cd4f992384b150c8662d9d11d0e85a10
joaquin_n6 <joaquinne87@gmail.com>
2024-12-09 12:54:30 -0600

    routes purchase

--- routes.py
+++ routes.py
@@ -5,8 +5,10 @@ from controllers import users_controller
 from controllers import product_controller
 from controllers import config_controller
 from controllers import supplier_controller
+from controllers import purchase_controller
 router = APIRouter()
 router.include_router(config_controller.router)
 router.include_router(users_controller.router)
 router.include_router(product_controller.router)
 router.include_router(supplier_controller.router)
+router.include_router(purchase_controller.router)

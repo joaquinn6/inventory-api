0a1e6af0638fe3b6027787eab6cc40a983fa4fea
joaquin_n6 <joaquinne87@gmail.com>
2024-12-09 14:10:04 -0600

    token _id user return

--- models/token_model.py
+++ models/token_model.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, Field
 
 
 class Token(BaseModel):
+  id: str = Field(..., alias="_id")
   token: str = Field(...)
   email: str = Field(...)
   roles: list[str] = Field(...)

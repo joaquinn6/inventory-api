ed62302161b984d721820a1bb3df108018a5db8a
joaquin_n6 <joaquinne87@gmail.com>
2025-03-20 20:15:11 -0600

    user base entity

--- models/user_model.py
+++ models/user_model.py
@@ -1,16 +1,15 @@
 from pydantic import BaseModel, Field
 from datetime import datetime
 
+from models.entity import Entity
 
-class User(BaseModel):
-  id: str = Field(..., alias="_id")
+
+class User(Entity):
   email: str = Field(...)
   roles: list[str] = Field(...)
   full_name: str = Field(...)
   password: str = Field(...)
   active: bool = Field(...)
-  created_at: datetime = None
-  updated_at: datetime = None
 
 
 class UserInfo(BaseModel):
